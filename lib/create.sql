75674fe1535c714979e87e3e26b818c97c5cd254
Jay Bednar <ironviralmedia@gmail.com>
2018-03-09 21:59:55 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE project (
+  id INTEGER PRIMARY TEXT,
+  title = TEXT,
+  category = TEXT,
+  funding_goal = INTEGER,
+  start_date = TEXT,
+  end_date = TEXT
+);
+
+CREATE TABLE user (
+  id INTEGER PRIMARY TEXT,
+  name = TEXT,
+  age = INTEGER
+);
