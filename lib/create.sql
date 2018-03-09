bfacdf3ceb178e00933fe9c9b802d528cfa139bd
Jay Bednar <ironviralmedia@gmail.com>
2018-03-09 22:01:03 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,21 @@
-CREATE TABLE project (
-  id INTEGER PRIMARY TEXT,
-  title = TEXT,
-  category = TEXT,
-  funding_goal = INTEGER,
-  start_date = TEXT,
-  end_date = TEXT
-);
-
-CREATE TABLE user (
-  id INTEGER PRIMARY TEXT,
-  name = TEXT,
-  age = INTEGER
-);
-
-CREATE TABLE pledge (
-  id INTEGER PRIMARY TEXT,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER 
-);
+-- CREATE TABLE project (
+--   id INTEGER PRIMARY TEXT,
+--   title = TEXT,
+--   category = TEXT,
+--   funding_goal = INTEGER,
+--   start_date = TEXT,
+--   end_date = TEXT
+-- );
+--
+-- CREATE TABLE user (
+--   id INTEGER PRIMARY TEXT,
+--   name = TEXT,
+--   age = INTEGER
+-- );
+--
+-- CREATE TABLE pledge (
+--   id INTEGER PRIMARY TEXT,
+--   amount INTEGER,
+--   user_id INTEGER,
+--   project_id INTEGER
+-- );
